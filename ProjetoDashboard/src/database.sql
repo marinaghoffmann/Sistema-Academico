7fe3e58060666047fcd99e1a769212d3b3060bba
Marina Hoffmann Guimarães <167432472+marinaghoffmann@users.noreply.github.com>
2025-09-26 21:50:41 -0300

    add tuplas

--- ProjetoDashboard/src/database.sql
+++ ProjetoDashboard/src/database.sql
@@ -48,7 +48,7 @@ CREATE TABLE Disciplina (
 
 INSERT INTO Aluno (Nome, Idade, Sexo, Media, Frequencia, Rua, Num, CEP, Monitor, Monitorado) 
 VALUES ('Marina Hoffmann', 19, 'F', 8.2, 94.49, 'Avenida Presidente Kennedy', 263, '54420-050', NULL, NULL),
-       ('Maria Julia Gomes', 20, 'F', 9.0, 70.0, 'Rua Dom Pedro II', 765, '05032-110', NULL, NULL);
+       ('Maria Julia Gomes', 20, 'F', 9.0, 70.0, 'Rua Dom Pedro II', 765, '05032-110', NULL, NULL),
        ('Tiago Cavalcanti', 43, 'M', 8.0, 100.0, 'Rua Benjamin Constant', 248, '08112-890', NULL, NULL),
        ('Julia Moscariello Câmara', 19, 'F', 9.0, 95.0, 'Rua São Pedro', 214, '04012-030', NULL, NULL),
        ('Júlia Araújo', 19, 'F', 8.0, 100.0, 'Rua Amendoim', 505, '29257-450', NULL, NULL),
@@ -90,15 +90,113 @@ VALUES ('Marina Hoffmann', 19, 'F', 8.2, 94.49, 'Avenida Presidente Kennedy', 26
 
 INSERT INTO Professor (CPF, Nome, Rua, Num, CEP)
 VALUES ('123.456.789-01', 'Izabel Malheiros', 'Rua das Acácias', 415, '31045-210'),
-       ('234.567.890-12', 'Walter Santos', 'Avenida Brasil', 782, '32056-430');
+       ('234.567.890-12', 'Walter Santos', 'Avenida Brasil', 782, '32056-430'),
+       ('345.678.901-23', 'Diogo Vieira', 'Rua Floriano Peixoto', 156, '33067-540'),
+       ('456.789.012-34', 'Mário Mazullo', 'Rua Dom Pedro II', 921, '34078-650'),
+       ('567.890.123-45', 'Luciana Santos', 'Rua Marechal Deodoro', 348, '35089-760'),
+       ('678.901.234-56', 'Wellington Estima', 'Rua da Aurora', 507, '36090-870'),
+       ('789.012.345-67', 'Nísia Aragão', 'Avenida Recife', 113, '37012-980'),
+       ('890.123.456-78', 'Paulo Wendel', 'Rua das Palmeiras', 664, '38023-090'),
+       ('901.234.567-89', 'Davi Morato', 'Rua Frei Caneca', 275, '39034-200'),
+       ('012.345.678-90', 'Ulisses Muliterno', 'Rua Santa Cruz', 842, '40045-310'),
+       ('135.246.357-91', 'Luiza Xavier', 'Rua Monte Castelo', 129, '41056-420'),
+       ('246.357.468-02', 'Marina Yanes', 'Rua Sete de Setembro', 903, '42067-530'),
+       ('357.468.579-13', 'Natan Zico', 'Rua José Bonifácio', 311, '43078-640'),
+       ('468.579.680-24', 'Otavio Alves', 'Rua Benjamin Constant', 742, '44089-750'),
+       ('579.680.791-35', 'Patricia Borges', 'Rua Santo Antônio', 258, '45090-860'),
+       ('680.791.802-46', 'Rubens Castro', 'Rua Joaquim Nabuco', 634, '46012-970'),
+       ('791.802.913-57', 'Sonia Duarte', 'Avenida Independência', 487, '47023-080'),
+       ('802.913.024-68', 'Tiago Esteves', 'Rua Padre Cícero', 322, '48034-190'),
+       ('913.024.135-79', 'Ursula Fonseca', 'Rua Bela Vista', 759, '49045-200'),
+       ('024.135.246-80', 'Vitor Guimarães', 'Rua Dom Bosco', 188, '50056-310'),
+       ('135.246.357-91', 'Wanda Henriques', 'Rua São João', 594, '51067-420'),
+       ('246.357.468-02', 'Xena Igor', 'Rua Projetada Leste', 401, '52078-530'),
+       ('357.468.579-13', 'Yasmin Jorge', 'Rua Getúlio Vargas', 222, '53089-640'),
+       ('468.579.680-24', 'Zeze Kilber', 'Rua das Flores', 845, '54090-750'),
+       ('579.680.791-35', 'Alana Lopes', 'Rua Padre Anchieta', 503, '55012-860'),
+       ('660.791.102-46', 'Bianca Mendes', 'Rua São Pedro', 310, '56023-970'),
+       ('791.802.913-57', 'Carlos Nogueira', 'Avenida Tiradentes', 775, '57034-080'),
+       ('802.913.024-68', 'Dora Ornelas', 'Rua José do Patrocínio', 492, '58045-190'),
+       ('913.024.135-79', 'Eduardo Pires', 'Rua da Liberdade', 156, '59056-200'),
+       ('024.135.246-80', 'Fatima Queiroz', 'Rua Marechal Hermes', 689, '60067-310');
+
 
 
 INSERT INTO Turma (Nome, Ano)
-VALUES ('Turma A', 2023), ('Turma B', 2023);
+VALUES ('Turma A', 2023), 
+       ('Turma B', 2023),
+       ('Turma C', 2023),
+       ('Turma D', 2023),
+       ('Turma E', 2023),
+       ('Turma F', 2023),
+       ('Turma G', 2023),
+       ('Turma H', 2023),
+       ('Turma I', 2023),
+       ('Turma J', 2023),
+       ('Turma K', 2024),
+       ('Turma L', 2024),
+       ('Turma M', 2024),
+       ('Turma N', 2024),
+       ('Turma O', 2024),
+       ('Turma P', 2024),
+       ('Turma Q', 2024),
+       ('Turma R', 2024),
+       ('Turma S', 2024),
+       ('Turma T', 2024),
+       ('Turma U', 2024),
+       ('Turma V', 2024),
+       ('Turma W', 2024),
+       ('Turma X', 2024),
+       ('Turma Y', 2024),
+       ('Turma Z', 2024),
+       ('Turma A1', 2025),
+       ('Turma B1', 2025),
+       ('Turma C1', 2025),
+       ('Turma D1', 2025);
+
+
 
 INSERT INTO Disciplina (Nome, Carga_Horaria, Id_Aluno, Id_Turma)
 VALUES ('Matemática', 60, 1, 1), 
-       ('Física', 45, 2, 2);
+       ('Física', 45, 2, 2),
+       ('Química', 50, 3, 3), 
+       ('Biologia', 60, 4, 4),
+       ('História', 40, 5, 5), 
+       ('Geografia', 40, 6, 6), 
+       ('Português', 80, 7, 7), 
+       ('Inglês', 40, 8, 8),
+       ('Artes', 30, 9, 9), 
+       ('Educação Física', 30, 10, 10), 
+       ('Sociologia', 45, 11, 11), 
+       ('Filosofia', 45, 12, 12),
+       ('Literatura', 60, 13, 13), 
+       ('Redação', 40, 14, 14), 
+       ('Programação', 80, 15, 15), 
+       ('Banco de Dados', 60, 16, 16),
+       ('Sistemas Operacionais', 60, 17, 17), 
+       ('Redes de Computadores', 45, 18, 18), 
+       ('Cálculo', 90, 19, 19), 
+       ('Geometria', 60, 20, 20),
+       ('Física Moderna', 50, 21, 21), 
+       ('Genética', 50, 22, 22), 
+       ('Microeconomia', 45, 23, 23), 
+       ('Macroeconomia', 45, 24, 24),
+       ('Estatística', 60, 25, 25), 
+       ('Introdução ao Direito', 40, 26, 26), 
+       ('Psicologia', 50, 27, 27), 
+       ('Contabilidade', 60, 28, 28),
+       ('Marketing Digital', 45, 29, 29), 
+       ('Gestão de Projetos', 60, 30, 30), 
+       ('Ética', 65, 31, 31), 
+       ('Anatomia', 40, 32, 32),
+       ('Espanhol', 50, 33, 33), 
+       ('Programação Orientada à Objetos', 55, 34, 34), 
+       ('Empreendedorismo', 60, 35, 35), 
+       ('Infraestrutura de Software', 70, 36, 36),
+       ('Infraestrutura de Hardware', 80, 37, 37), 
+       ('Biologia Marítma', 40, 38, 38), 
+       ('Libras', 60, 39, 39);
+
 
 DELETE FROM Aluno WHERE Id_Aluno = 2;
 
